569dc2ee3af69900a81558bf8248937aa99f3bcb
Oleksandr Nikolaiev <oleksandr.nikolaiev@ciellos.com>
2024-05-09 12:56:56 +0300

    Update wiki (#20)

--- build/update-docs.ps1
+++ build/update-docs.ps1
@@ -1,6 +1,6 @@
 # Script to generate the comment based markdown help files.
 # based on https://gist.github.com/Splaxi/8934e13cb35918d13af6e3a21c208b0e
-# See also https://github.com/d365collaborative/d365fo.tools/wiki/Building-tools
+# See also https://github.com/fscpscollaborative/fscps.tools/wiki/Building-tools
 $path = "$PSScriptRoot\.."
 
 Import-Module "$path\fscps.tools" -Force
