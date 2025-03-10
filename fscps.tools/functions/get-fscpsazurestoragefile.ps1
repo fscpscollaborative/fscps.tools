ea76578837133304115f30472bb2bbd0745f9abc
Oleksandr Nikolaiev <oleksandr.nikolaiev@ciellos.com>
2025-03-10 22:15:11 +0100

    Release merge. Refactor Get-FSCPSAzureStorageFile (#130) (#131)
    * Refactor Get-FSCPSAzureStorageFile (#130)
    
    * add internal function Get-ParameterValues
    
    Adds the capability to determine the parameters and their values a function is being executed with.
    
    * refactor Get-FSCPSAzureStorageFile as wrapper for d365fo.tools
    
    Instead of reimplementing functions from d365fo.tools, this functions now acts as a wrapper for those functions.
    
    * Update docs (#132)
    
    ---------
    
    Co-authored-by: Florian Hopfner <33372796+FH-Inway@users.noreply.github.com>

--- fscps.tools/functions/get-fscpsazurestoragefile.ps1
+++ fscps.tools/functions/get-fscpsazurestoragefile.ps1
@@ -15,9 +15,6 @@
     .PARAMETER SAS
         The SAS key that you have created for the storage account or blob container
         
-    .PARAMETER DestinationPath
-        The destination folder of the Azure file to download. If empty just show the file information
-        
     .PARAMETER Container
         Name of the blob container inside the storage account where you want to look for files
         
@@ -27,6 +24,9 @@
         Accepts wildcards for searching. E.g. -Name "Application*Adaptor"
         
         Default value is "*" which will search for all files
+        
+    .PARAMETER DestinationPath
+        The destination folder of the Azure file to download. If empty just show the file information
         
     .PARAMETER Latest
         Instruct the cmdlet to only fetch the latest file from the Azure Storage Account
@@ -65,31 +65,10 @@
     .NOTES
         Tags: Azure, Azure Storage, Token, Blob, File, Container
         
-        This is refactored function from d365fo.tools (https://github.com/d365collaborative/d365fo.tools) under the
-        MIT License
+        This is a wrapper for the d365fo.tools functions Get-D365AzureStorageFile and Invoke-D365AzureStorageDownload to enable both retrieving file information from an Azure Storage Account and donwloading the files.
         
-        Copyright (c) 2018 Mötz Jensen & Rasmus Andersen
-        
-        Permission is hereby granted, free of charge, to any person obtaining a copy
-        of this software and associated documentation files (the "Software"), to deal
-        in the Software without restriction, including without limitation the rights
-        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-        copies of the Software, and to permit persons to whom the Software is
-        furnished to do so, subject to the following conditions:
-        
-        The above copyright notice and this permission notice shall be included in all
-        copies or substantial portions of the Software.
-        
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        SOFTWARE.
-        
-        Original Author: Mötz Jensen (@Splaxi)
         Author: Oleksandr Nikolaiev (@onikolaiev)
+        Author: Florian Hopfner (@FH-Inway)
 #>
 function Get-FSCPSAzureStorageFile {
     [CmdletBinding(DefaultParameterSetName = 'Default')]
@@ -121,56 +100,51 @@ function Get-FSCPSAzureStorageFile {
         Invoke-TimeSignal -Start
     }
     process{
+
         if (Test-PSFFunctionInterrupt) { return}
-        if (([string]::IsNullOrEmpty($AccountId) -eq $true) -or
-            ([string]::IsNullOrEmpty($Container)) -or
-            (([string]::IsNullOrEmpty($AccessToken)) -and ([string]::IsNullOrEmpty($SAS)))) {
-            Write-PSFMessage -Level Host -Message "It seems that you are missing some of the parameters. Please make sure that you either supplied them or have the right configuration saved."
-            Stop-PSFFunction -Message "Stopping because of missing parameters"
-            return
-        }
 
-        if ([string]::IsNullOrEmpty($SAS)) {
-            Write-PSFMessage -Level Verbose -Message "Working against Azure Storage Account with AccessToken"
-
-            $storageContext = New-AzStorageContext -StorageAccountName $AccountId.ToLower() -StorageAccountKey $AccessToken
-        }
-        else {
-            Write-PSFMessage -Level Verbose -Message "Working against Azure Storage Account with SAS"
-
-            $conString = $("BlobEndpoint=https://{0}.blob.core.windows.net/;QueueEndpoint=https://{0}.queue.core.windows.net/;FileEndpoint=https://{0}.file.core.windows.net/;TableEndpoint=https://{0}.table.core.windows.net/;SharedAccessSignature={1}" -f $AccountId.ToLower(), $SAS)
-            $storageContext = New-AzStorageContext -ConnectionString $conString
-        }
+        $params = Get-ParameterValue
+        if ($params.ContainsKey("DestinationPath")) { $null = $params.Remove("DestinationPath") }
+        $files = Get-D365AzureStorageFile @params
 
         try {
-            $files = Get-AzStorageBlob -Container $($Container.ToLower()) -Context $storageContext | Sort-Object -Descending { $_.LastModified }
+
+            $selectParams = @{
+                TypeName = "FSCPS.TOOLS.Azure.Blob"
+                Property =
+                    "Name",
+                    "Size",
+                    "LastModified"
+            }
 
             if ($Latest) {
-                $files | Select-Object -First 1 | Select-PSFObject -TypeName FSCPS.TOOLS.Azure.Blob "name", @{Name = "Size"; Expression = { [PSFSize]$_.Length } }, @{Name = "LastModified"; Expression = { $_.LastModified.UtcDateTime } }
+                $files | Select-Object -First 1 | Select-PSFObject @selectParams
+            }
+            elseif ($DestinationPath) {
+                $d365AzureStorageDownloadParams = Get-DeepClone $params
+                $d365AzureStorageDownloadParams.Force = $true
+                foreach ($obj in $files) {
+                    $null = Test-PathExists -Path $DestinationPath -Type Container -Create
+                    $d365AzureStorageDownloadParams.Name = $obj.Name
+                    $d365AzureStorageDownloadParams.Path = $DestinationPath
+                    $null = Invoke-D365AzureStorageDownload @d365AzureStorageDownloadParams
+                    $destinationBlobPath = (Join-Path $DestinationPath ($obj.Name))
+                    $selectParams.Property =
+                        "Name",
+                        "Size",
+                        @{Name = "Path"; Expression = { [string]$destinationBlobPath } },
+                        "LastModified"
+                    $obj | Select-PSFObject @selectParams
+                }
             }
             else {
-        
-                foreach ($obj in $files) {
-                    if ($obj.Name -NotLike $Name) { continue }
-
-                    if ($DestinationPath) {
-                        $null = Test-PathExists -Path $DestinationPath -Type Container -Create
-                        $destinationBlobPath = (Join-Path $DestinationPath ($obj.Name))
-                        Get-AzStorageBlobContent -Context $storageContext -Container $($Container.ToLower()) -Blob $obj.Name -Destination ($destinationBlobPath) -ConcurrentTaskCount 10 -Force
-                        $obj | Select-PSFObject -TypeName FSCPS.TOOLS.Azure.Blob "name", @{Name = "Size"; Expression = { [PSFSize]$_.Length } }, @{Name = "Path"; Expression = { [string]$destinationBlobPath } }, @{Name = "LastModified"; Expression = { $_.LastModified.UtcDateTime } }
-                    }
-                    else {
-                        $obj | Select-PSFObject -TypeName FSCPS.TOOLS.Azure.Blob "name", @{Name = "Size"; Expression = { [PSFSize]$_.Length } }, @{Name = "LastModified"; Expression = { $_.LastModified.UtcDateTime } }
-                    }
-                    
-                    
-                }
+                $files | Select-PSFObject @selectParams
             }
         }
         catch {
             Write-PSFMessage -Level Warning -Message "Something broke" -ErrorRecord $_
         }
-    }   
+    }
 
     end {
         Invoke-TimeSignal -End
