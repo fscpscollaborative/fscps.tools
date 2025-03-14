56cc36fc6155e8a3dcacba0d34a440df550e70e8
Florian Hopfner <florian.hopfner@inway.de>
2025-03-14 18:48:08 +0100

    make Get-MediaFileTypeByfileName an internal function
    Functions in helpers.ps1 are not included by bsts-build.ps1 in commands.ps1. Instead, they need to be loaded individually when they are used (see for example Update-FSCPSISVSource).

--- fscps.tools/internal/scripts/helpers.ps1
+++ fscps.tools/internal/scripts/helpers.ps1
@@ -241,35 +241,3 @@ function Expand-7zipArchive {
         Expand-Archive -Path $Path -DestinationPath "$DestinationPath" -Force
     }
 }
-
-function Get-MediaTypeByFilename {
-    [CmdletBinding()]
-    param(
-      [Parameter(Mandatory, ValueFromPipeline)]
-      [string[]] $Filename
-    )
-    begin {
-      # Download and parse the list of media types (MIME types) via
-      # https://github.com/jshttp/mime-db.
-      # NOTE:
-      #  * For better performance consider caching the JSON file.
-      #  * A fixed release is targeted, to ensure that future changes to the JSON
-      #    format do not break the command.
-      $mediaTypes = (
-        Invoke-RestMethod https://cdn.jsdelivr.net/gh/jshttp/mime-db@v1.53.0/db.json
-      ).psobject.Properties
-    }
-    process {
-      foreach ($name in $Filename) {
-        # Find the matching media type by filename extension.
-        $matchingMediaType =
-          $mediaTypes.Where(
-            { $_.Value.extensions -contains [IO.Path]::GetExtension($name).Substring(1) },
-            'First'
-          ).Name
-        # Use a fallback type, if no match was found.
-        if (-not $matchingMediaType) { $matchingMediaType = 'application/octet-stream' }
-        $matchingMediaType # output
-      }
-    }
-}
