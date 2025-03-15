90bdd5f29ce80a8da450eaf29de9f2d3d1ca0bbe
Florian Hopfner <florian.hopfner@inway.de>
2025-03-15 16:35:35 +0100

    cache mediatypes db.json file locally

--- fscps.tools/internal/functions/get-mediafiletypebyfilename.ps1
+++ fscps.tools/internal/functions/get-mediafiletypebyfilename.ps1
@@ -21,33 +21,33 @@
         Author: Florian Hopfner (@FH-Inway)
 #>
 function Get-MediaTypeByFilename {
-  [CmdletBinding()]
-  param(
-    [Parameter(Mandatory, ValueFromPipeline)]
-    [string[]] $Filename
-  )
-  begin {
-    # Download and parse the list of media types (MIME types) via
-    # https://github.com/jshttp/mime-db.
-    # NOTE:
-    #  * For better performance consider caching the JSON file.
-    #  * A fixed release is targeted, to ensure that future changes to the JSON
-    #    format do not break the command.
-    $mediaTypes = (
-      Invoke-RestMethod https://cdn.jsdelivr.net/gh/jshttp/mime-db@v1.53.0/db.json
-    ).psobject.Properties
-  }
-  process {
-    foreach ($name in $Filename) {
-      # Find the matching media type by filename extension.
-      $matchingMediaType =
-        $mediaTypes.Where(
-          { $_.Value.extensions -contains [IO.Path]::GetExtension($name).Substring(1) },
-          'First'
-        ).Name
-      # Use a fallback type, if no match was found.
-      if (-not $matchingMediaType) { $matchingMediaType = 'application/octet-stream' }
-      $matchingMediaType # output
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [string[]] $Filename
+    )
+    begin {
+        # Download and parse the list of media types (MIME types) via
+        # https://github.com/jshttp/mime-db.
+        # NOTE: A fixed release is targeted, to ensure that future changes to the JSON format do not break the command.
+        if (-not (Test-PathExists -Path $Script:MediaTypesPath -Type Leaf -WarningAction SilentlyContinue)) {
+            $mediaTypesFolder = Split-Path -Path $Script:MediaTypesPath -Parent
+            $null = Test-PathExists -Path $mediaTypesFolder -Type Container -Create
+            Invoke-RestMethod -Uri https://cdn.jsdelivr.net/gh/jshttp/mime-db@v1.53.0/db.json -OutFile $Script:MediaTypesPath
+        }
+        $mediaTypes = (Get-Content -Path $Script:MediaTypesPath | ConvertFrom-Json).psobject.Properties
     }
-  }
+    process {
+        foreach ($name in $Filename) {
+            # Find the matching media type by filename extension.
+            $matchingMediaType =
+                $mediaTypes.Where(
+                    { $_.Value.extensions -contains [IO.Path]::GetExtension($name).Substring(1) },
+                    'First'
+                ).Name
+            # Use a fallback type, if no match was found.
+            if (-not $matchingMediaType) { $matchingMediaType = 'application/octet-stream' }
+            $matchingMediaType # output
+        }
+    }
 }
